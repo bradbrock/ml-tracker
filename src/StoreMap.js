1ca180181bc9bc06bdc6eeb7109c070498be147a
Brad Brock <bbrock@fossil.com>
2018-10-09 11:14:03 -0500

    Unique keys for marker objects

--- src/StoreMap.js
+++ src/StoreMap.js
@@ -3,29 +3,21 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 
 const StoreMap = withScriptjs(withGoogleMap((props) => {
-  
-  // { console.log(props.markerData.data[0].locations) }
-
-  // { props.markerData.data[0].locations.map((i) => {
-  //   console.log("lat " + i.lat)
-  //   console.log("lng " + i.lng)
-  // })}
 
   return (
     <GoogleMap
       defaultZoom={14}
-      // center= {{lat: 42.3601, lng: -71.0589}}
       center = { props.location }
     >
       <Marker
         position={{ lat: 42.3601, lng: -71.0589 }}
         labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
       />
-      { props.markerData.data[0].locations.map((i) => {
+      { props.markerData.data[0].locations.map((marker, i) => {
         return (
           <Marker
             key={i}
-            position={{ lat: i.lat, lng: i.lng }}
+            position={{ lat: marker.lat, lng: marker.lng }}
           />
         )
       })}
